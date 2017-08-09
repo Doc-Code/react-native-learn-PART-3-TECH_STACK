ee958e9289873de5c73dec739f947105801281f1
Dr.Code <stweb.it@gmail.com>
2017-08-09 17:53:21 +1000

    вот тут я вообще них**я не понял
    выводи ListView

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import { ListView } from 'react-native0;'
 import { connect } from 'react-redux';
 
  class LibraryList extends Component {
+   componentWillMount() {
+     const ds = new ListView.DataSource({
+       rowHasChanged: (r1, r2) => r1 !== r2
+     });
+
+     this.dataSorce = ds.cloneWithRows(this.props.libraries);
+   }
+
+   renderRow {
+     
+   }
+
    render() {
-     console.log(this.props);
-     return;
+     return (
+       <ListView
+         dataSorce={this.dataSorce}
+         renderRow={this.renderRow}
+       />
+     );
    }
  }
 
